c99e62289f4c3c62fabf63b201e754bc750eeacd
Mathieu-Poirier <mathieuworkemail@gmail.com>
2023-10-17 17:45:17 -0400

    Printed table from database

--- inventory_api/routes/visualization.js
+++ inventory_api/routes/visualization.js
@@ -1,10 +1,25 @@
 // routes/sampleRoute.js
 const express = require('express');
 const router = express.Router();
+const { Pool } = require('pg');
+
+// Create a PostgreSQL connection pool
+const pool = new Pool({
+  user: 'postgres',
+  host: '127.0.0.1',
+  database: 'postgres',
+  password: 'yo',
+  port: 5432, // Default PostgreSQL port
+});
+
+
+
 
 // Define a route
-router.get('/visualization', (req, res) => {
-  res.send('This is the visualization route.');
+router.get('/visualization', async (req, res) => {
+    const result = await pool.query('SELECT parts_made FROM efficiency');
+    // Send the query result as a JSON response
+    res.json(result.rows);
 });
 
 module.exports = router;
